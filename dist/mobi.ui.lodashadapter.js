3160678912e683fa81ed767f22ba6b94615c8612
U110730 <ueli.loetscher@mobi.ch>
2014-12-15 17:51:51 +0100

    improved build

---
+++ dist/mobi.ui.lodashadapter.js
@@ -0,0 +1,10 @@
+/* mobi.ui.lodashadapter - v0.0.2 - 2014-12-15 */
+
+angular.module('mobi.ui.lodashadapter', [
+    'mobi.ui.lodashadapter.services'
+]);
+
+angular.module('mobi.ui.lodashadapter.services', [])
+    .factory('_', function ($window) {
+        return $window._;
+    });
